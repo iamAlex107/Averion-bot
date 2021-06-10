e138f0b59a92cbebad928a15f867315753382074
iamAlex107 <85471464+iamAlex107@users.noreply.github.com>
2021-06-10 18:53:09 +0530

    ✔ Fixed a minor bug in Help Command !!

--- src/commands/info/info.js
+++ src/commands/info/info.js
@@ -9,7 +9,7 @@ module.exports = {
         .addField(`Developer:`, `<a:Ez_Arrow:851020973690978324> **Name:** Alex\n<a:Ez_Arrow:851020973690978324> **Discord Tag:** iamAlex#9999\n<a:Ez_Arrow:851020973690978324> **Description:** Rookie Bot Developer, Gamer and Coder\n<a:Ez_Arrow:851020973690978324> **Birthday:** 6th February 2006`)
         .addField(`Bot Info:`, `<a:Ez_Arrow:851020973690978324> **Language used:** JavaScript (JS)\n<a:Ez_Arrow:851020973690978324> **Ping:** ${client.ws.ping} ms\n<a:Ez_Arrow:851020973690978324> **Invite Link:** [Invite link](https://discord.com/api/oauth2/authorize?client_id=851725121164541972&permissions=4227329783&scope=bot)\n<a:Ez_Arrow:851020973690978324> **GitHub:** [GitHub](https://github.com/iamAlex107/Averion-bot/tree/main)\n<a:Ez_Arrow:851020973690978324> **Prefix:** a!`)
         .setColor(`#f54a00`)
-        .setFooter(`Made with 💚 by iamAlex#9999`)
+        .setFooter(`Made with 💚 by iamAlex#9999`, message.author.displayAvatarURL({ dynamic: true }))
         .setTimestamp()
         message.channel.send(embed)
     }
