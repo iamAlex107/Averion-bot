bbc7bc4a5fef6f45f41a99f86a8fd834b33ac3e7
iamAlex107 <85471464+iamAlex107@users.noreply.github.com>
2021-06-28 19:32:39 +0530

    ✔ Made help compatible with all new Commands!

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -9,14 +9,14 @@ module.exports = {
     run: async(client, message)=>{
         let embed = new Discord.MessageEmbed()
         .setTitle(`Hey ${message.author.username}! Here are the commands :`)
-        .setDescription(`**Page 1:** Index\n**Page 2:** Info\n**Page 3:** Fun`)
+        .setDescription(`**Page 1:** Index\n**Page 2:** Info\n**Page 3:** Fun\n**Page 4:** Moderation`)
         .setColor(`#f5f500`)
         .setFooter(`Made with 💚 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
         .setTimestamp()
 
         let embed1 = new Discord.MessageEmbed()
         .setTitle(`Info :`)
-        .setDescription(`<a:Arrow:851020973690978324> **ping** --> Shows the Bot ping\n<a:Arrow:851020973690978324> **info** --> Shows bot and developer information\n<a:Arrow:851020973690978324> **prefix** --> Shows the bot prefix\n<a:Arrow:851020973690978324> **uptime** --> Shows the uptime of the bot (alias: **online**)\n<a:Arrow:851020973690978324> **avatar** --> Shows the avatar of the user (alias: **av**)\n<a:Arrow:851020973690978324> **help** --> Shows the help page`)
+        .setDescription(`<a:Arrow:851020973690978324> **ping** --> Shows the Bot ping\n<a:Arrow:851020973690978324> **info** --> Shows bot and developer information\n<a:Arrow:851020973690978324> **prefix** --> Shows the bot prefix\n<a:Arrow:851020973690978324> **uptime** --> Shows the uptime of the bot (alias: **online**)\n<a:Arrow:851020973690978324> **serverinfo** --> Shows Guild information (alias: **si**, **guildinfo**)\n<a:Arrow:851020973690978324> **avatar** --> Shows the avatar of the user (alias: **av**)\n<a:Arrow:851020973690978324> **help** --> Shows the help page`)
         .setColor(`#f59b00`)
         .setFooter(`Made with 💚 by iamAlex#1306`,message.author.displayAvatarURL({ dynamic: true }))
         .setTimestamp()
@@ -28,7 +28,14 @@ module.exports = {
         .setFooter(`Made with 💚 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
         .setTimestamp()
 
-        let pages = [embed, embed1, embed2]
+        let embed3 = new Discord.MessageEmbed()
+        .setTitle(`Moderation`)
+        .setDescription(`<a:Arrow:851020973690978324> **ban** --> Ban's the targeted User !\n<a:Arrow:851020973690978324> **kick** --> Kick's the targeted User !`)
+        .setColor(`#f54a00`)
+        .setFooter(`Made with 💚 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+
+        let pages = [embed, embed1, embed2, embed3]
         ReactionPages(message, pages, false)
     }
 }
