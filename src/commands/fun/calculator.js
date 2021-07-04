d046e501fef486e77f5b5c340321261d4afe5888
iamAlex107 <85471464+iamAlex107@users.noreply.github.com>
2021-07-04 18:59:26 +0530

    ✅New Command

---
+++ src/commands/fun/calculator.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const math = require('math-expression-evaluator');
+
+module.exports = {
+    name: "calculator",
+    aliases: ["calc", "math"],
+    run: async(client, message, args)=>{
+        if (args.length < 1) return message.channel.send(`<a:crossGif:853559074833301504> **Please a provide a question to be solved on the Calculator !**`)
+
+        const question = args.join(' ');
+        let answer = math.eval(question);
+        if (isNaN(question)){
+            let a = 0; // Overriding if to let embed directly executed
+        }
+
+        
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Here is your answer")
+        .setColor(`#32F32F`)
+        .setDescription(`**Question:**\n${question}\n**Answer:**\n${answer}`)
+        .setFooter(`Get help with your math LOL`, message.author.displayAvatarURL({dynamic: true}))
+        .setTimestamp()
+
+        message.channel.send(embed)
+        
+    }
+}
+// let errorEmbed = new Discord.MessageEmbed()
+//             .setTitle(`ERROR`)
+//             .setDescription(`Invalid Math Equation`)
+//             .setColor(`#32F32F`)
+//             .setFooter(`😑`, message.author.displayAvatarURL({dynamic: true}))
+//             .setTimestamp()
+
+//             message.channel.send(errorEmbed)
