9259cdc54a1638b85e24f0632efbf1b6c3a0a8a7
iamAlex107 <85471464+iamAlex107@users.noreply.github.com>
2021-07-04 18:51:29 +0530

    ✅Changed a few arguments !

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,51 +1,42 @@
-const discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 module.exports = {
-  name: "ban",
-  category: "moderation",
-  usage: "ban <@user> <reason>",
-  run: async (client, message, args) => {
-    
-    const target = message.mentions.members.first()
-    
-    const reason = args.slice(1).join(" ")
-    
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **I cannot ban this user !**`)
-    
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **You don't have the required permissions: Ban Member !**`)
-    
-    if(target === message.author) return message.channel.send("**You cannot ban yourself !**")
+    name: "ban",
+    description: "Bans the targeted user",
+    usage: "ban <@user> <reason>",
+    run: async(client, message, args)=>{
+        const target = message.mentions.members.first();
+        const reason = args.slice(1).join(" ");
 
-    if(!args[0]) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`)
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **You don't have the required permissions: Ban Member !**`);
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **I do not have permission to Ban !**`);
 
-    if(!reason) return message.channel.send(`<a:crossGif:853559074833301504> **Please Provide a Reason !**`)
-    
-    if(!target) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`)
-    
-    if(target.roles.highest.position >= message.member.roles.highest.position) {
-      return message.channel.send(`<a:crossGif:853559074833301504> **You cannot ban this user !**`)
+        if(!args[0]) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`);
+        if(!target) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`);
+
+        if(!reason) return message.channel.send(`<a:crossGif:853559074833301504> **Please Provide a Reason !**`);
+
+        if(target.id === message.author.id) return message.channel.send("<a:crossGif:853559074833301504> **You cannot ban yourself !**");
+        if(target.roles.highest.position >= message.member.roles.highest.position) {
+            return message.channel.send(`<a:crossGif:853559074833301504> **You cannot ban this user !**`)
+        };
+
+        if(target.bannable){
+            let embed = new Discord.MessageEmbed()
+            .setTitle(`Member was successfully Banned`)
+            .setColor(`#ff1100`)
+            .setDescription(`<a:Arrow:851020973690978324> **Target:** ${target}\n<a:Arrow:851020973690978324> **Reason:** ${reason}\n<a:Arrow:851020973690978324> **Moderator:** ${message.author.tag}`)
+            .setFooter(`Made with 💙 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
+            .setTimestamp()
+
+            message.channel.send(embed);
+            target.ban();
+            message.delete();
+        }else {
+            return message.channel.send(`<a:crossGif:853559074833301504> **I was unable to ban the user !**`)
+        }
+        return undefined;
+
     }
-    
-    
-    if(target.bannable) {
-      let embed = new discord.MessageEmbed()
-      .setTitle(`Member was successfully banned`)
-      .setColor("#00ffff")
-      .addField('<a:Arrow:851020973690978324> Target',`${target}`)
-      .addField('<a:Arrow:851020973690978324> Reason',`${reason}`)
-      .addField('<a:Arrow:851020973690978324> Moderator',`${message.author.tag}`)
-      .setFooter(`Made with 💙 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
-      .setTimestamp()
-      
-      message.channel.send(embed)
-      
-      target.ban()
-      
-      message.delete()
-      
-    } else {
-      return message.channel.send(`<a:crossGif:853559074833301504> **I was unable to ban the user !**`)
-    }
-    return undefined
-  }
-};
+}
--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,51 +1,44 @@
-const discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 module.exports = {
-  name: "kick",
-  category: "moderation",
-  usage: "kick <@user> <reason>",
-  run: async (client, message, args) => {
-    
-    const target = message.mentions.members.first()
-    
-    const reason = args.slice(1).join(" ")
-    
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **I cannot kick this user !**`)
-    
-    if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **You don't have the required permissions: Kick Member !**`)
-    
-    if(target === message.author) return message.channel.send("**You cannot kick yourself !**")
+    name: "kick",
+    description: "Kicks the targeted user",
+    usage: "kick <@user> <reason>",
+    run: async(client, message, args)=>{
+        const target = message.mentions.members.first();
+        var reason = args.slice(1).join(" ");
 
-    if(!args[0]) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`)
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **You don't have the required permissions: Kick Member !**`);
+        if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send(`<a:crossGif:853559074833301504> **I do not have permission to Ban !**`);
 
-    if(!reason) return message.channel.send(`<a:crossGif:853559074833301504> **Please Provide a Reason !**`)
-    
-    if(!target) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`)
-    
-    if(target.roles.highest.position >= message.member.roles.highest.position) {
-      return message.channel.send(`<a:crossGif:853559074833301504> **You cannot kick this user !**`)
+        if(!args[0]) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`);
+        if(!target) return message.channel.send(`<a:crossGif:853559074833301504> **Please Mention a User !**`);
+
+        if(!reason){
+            reason = '`None Provided`';
+        }
+
+        if(target.id === message.author.id) return message.channel.send("<a:crossGif:853559074833301504> **You cannot kick yourself !**");
+        if(target.roles.highest.position >= message.member.roles.highest.position) {
+            return message.channel.send(`<a:crossGif:853559074833301504> **You cannot kick this user !**`)
+        };
+
+        if(target.bannable){
+            let embed = new Discord.MessageEmbed()
+            .setTitle(`Member was successfully Kicked`)
+            .setColor(`#ff1100`)
+            .setDescription(`<a:Arrow:851020973690978324> **Target:** ${target}\n<a:Arrow:851020973690978324> **Reason:** ${reason}\n<a:Arrow:851020973690978324> **Moderator:** ${message.author.tag}`)
+            .setFooter(`Made with 💙 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
+            .setTimestamp()
+
+            message.channel.send(embed);
+            target.kick();
+            message.delete();
+        }else {
+            return message.channel.send(`<a:crossGif:853559074833301504> **I was unable to kick the user !**`)
+        }
+        return undefined;
+
     }
-    
-    
-    if(target.kickable) {
-      let embed = new discord.MessageEmbed()
-      .setTitle(`Member was successfully kicked`)
-      .setColor("#00ffff")
-      .addField('<a:Arrow:851020973690978324> Target',`${target}`)
-      .addField('<a:Arrow:851020973690978324> Reason',`${reason}`)
-      .addField('<a:Arrow:851020973690978324> Moderator',`${message.author.tag}`)
-      .setFooter(`Made with 💙 by iamAlex#1306`, message.author.displayAvatarURL({ dynamic: true }))
-      .setTimestamp()
-      
-      message.channel.send(embed)
-      
-      target.kick()
-      
-      message.delete()
-      
-    } else {
-      return message.channel.send(`<a:crossGif:853559074833301504> **I was unable to kick the user !**`)
-    }
-    return undefined
-  }
-};
+}
