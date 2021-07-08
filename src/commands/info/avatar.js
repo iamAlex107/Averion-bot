93bf8c01b9be0bb5d02a2f1f616bd48ff5dcc7f6
iamAlex107 <85471464+iamAlex107@users.noreply.github.com>
2021-07-08 09:45:15 +0530

    ✅ Fixed a Minor Bug
    Fixed a minor bug where the user's own avatar was not shown while using **a!av** only.

--- src/commands/info/avatar.js
+++ src/commands/info/avatar.js
@@ -1,36 +1,49 @@
-const { MessageEmbed, DiscordAPIError } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	name: 'avatar',
 	aliases: ['av'],
 	run: async(client, message, args)=>{
+		if (!args[0]){
+			let avatarURL = message.author.displayAvatarURL({
+				size: 4096,
+				dynamic: true
+			});
+			const embed = new MessageEmbed()
+			.setTitle(`**Avatar of ${message.author.tag} **`)
+			.setImage(avatarURL)
+			.setColor(`#00FFFF`)
+			.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
+			.setTimestamp();
+
+			message.channel.send(embed);
+		}
 
 		let userinfoget =
 			message.mentions.members.first() ||
 			await message.guild.members.fetch(args[0]) ||
 			message.guild.member(message.author);
 
-		let msg = await message.channel.send('**Generating avatar** 🍕...');
 
         let target = args[0];
 
-		try {
-			let targetCheck = parseInt(target);
+		let targetCheck = parseInt(target);
+		try{
 			if(!isNaN(targetCheck)){
-				let targetID = message.guild.members.cache.get(`${target}`);
-				let avatarURL = targetID.user.displayAvatarURL({
-					size: 4096,
-					dynamic: true
-				});
-				const embed = new MessageEmbed()
-				.setTitle(`**Avatar of ${targetID.user.tag} **`)
-				.setImage(avatarURL)
-				.setColor(`#00FFFF`)
-				.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
-				.setTimestamp();
+			let targetID = message.guild.members.cache.get(`${target}`);
+			let avatarURL = targetID.user.displayAvatarURL({
+				size: 4096,
+				dynamic: true
+			});
+			const embed = new MessageEmbed()
+			.setTitle(`**Avatar of ${targetID.user.tag} **`)
+			.setImage(avatarURL)
+			.setColor(`#00FFFF`)
+			.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
+			.setTimestamp();
 
-				message.channel.send(embed);
-				msg.delete();
+			message.channel.send(embed);
+			 
 			}else {
 				let targetMention = message.mentions.users.first();
 				let avatarURL = targetMention.displayAvatarURL({
@@ -45,26 +58,10 @@ module.exports = {
 				.setTimestamp();
 
 				message.channel.send(embed);
-				msg.delete();
+				 
 			}
-		} catch (err) {
-			let avatarURL = message.author.displayAvatarURL({
-				size: 4096,
-				dynamic: true
-			});
-			const embed = new MessageEmbed()
-			.setTitle(`**Avatar of ${message.author.tag} **`)
-			.setImage(avatarURL)
-			.setColor(`#00FFFF`)
-			.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
-			.setTimestamp();
-
-			message.channel.send(embed);
-			console.log(err)
-			msg.delete();
+		}catch (err){
+			message.channel.send(`Please provide a valid user`)
 		}
-
-
-
 	}
 }
